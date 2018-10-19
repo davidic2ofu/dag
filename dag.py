60ad5b9190afa0ac50962ca6fdb4d4006533c405
David Rosenberg <dcrsnbrg@memphis.edu>
2018-10-19 15:02:14 -0500

    updated for directed graph, finds and reports longest path, and if cyclic or not

--- dag.py
+++ dag.py
@@ -41,7 +41,7 @@ def get_edges():
 		a = input('\nEnter value of edge: ')
 		if not a:
 			break
-		edge = tuple(sorted([int(x.strip(',')) for x in a.split()]))
+		edge = tuple([int(x.strip(',')) for x in a.split()])
 		edges.add(edge)
 	return edges
 
@@ -52,23 +52,36 @@ def bellman_ford(vertices, edges):
 	for v in vertices:
 		dist[v] = 999999
 		prev[v] = None
-	print('vertices in bf:', dist, prev, edges)
+	# print('vertices in bf:', dist, prev, edges)
 	# s = vertices[random.randint(0, len(vertices) - 1)]
 	s = 1
 	dist[s] = 0
+	is_cyclic = False
 	for i in range(len(vertices) - 1):
 		for e in edges:
-			print('e', e[0], e[1])
 			u, v = e[0], e[1]
-			if dist[v] > dist[u] + 1 and dist[u] != 999999:
-				dist[v] = dist[u] + 1
+			if dist[v] > dist[u] - 1:   # subtract 1 to get longest path
+				if dist[v] < 999999:	# or add 1 to get shortest path
+					is_cyclic = True
+				dist[v] = dist[u] - 1
 				prev[v] = u
 	print('distances: {}\nprevious: {}\n'.format(dist, prev))
+	return dist, prev, is_cyclic
+
+
+def find_path(prev):
+	def _find_path(val):
+		return [val] if val == 1 else [val] + _find_path(prev[val])
+	return _find_path(len(prev))
 
 
 if __name__ == '__main__':
 	print(instructions)
 	vertices = get_vertices()
 	edges = get_edges()
-	bellman_ford(vertices, edges)
+	dist, prev, is_cyclic = bellman_ford(vertices, edges)
+	if is_cyclic:
+		print("NO... Graph is cyclic, therefore not a DAG")
+	path = find_path(prev)
+	print(path)
 
