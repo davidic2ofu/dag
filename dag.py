ca93ef2a1e8e0b0a9dc2df1e625ab725a4018418
David Rosenberg <dcrsnbrg@memphis.edu>
2018-10-19 13:47:52 -0500

    works for undirected graph

--- dag.py
+++ dag.py
@@ -1,3 +1,4 @@
+import random
 
 
 instructions = '''
@@ -29,22 +30,45 @@ INPUTS
 '''
 
 
-def get_num_vertices():
-	return int(input('\nEnter number of vertices: '))
+def get_vertices():
+	num_vertices = int(input('\nEnter number of vertices: '))
+	return [x for x in range(1, num_vertices + 1)]
 
 
 def get_edges():
-	edges = []
+	edges = set()
 	while True:
 		a = input('\nEnter value of edge: ')
 		if not a:
 			break
-		edge = tuple([x.strip(',') for x in a.split()])
-		edges.append(edge)
+		edge = tuple(sorted([int(x.strip(',')) for x in a.split()]))
+		edges.add(edge)
 	return edges
+
+
+def bellman_ford(vertices, edges):
+	dist = {}
+	prev = {}
+	for v in vertices:
+		dist[v] = 999999
+		prev[v] = None
+	print('vertices in bf:', dist, prev, edges)
+	# s = vertices[random.randint(0, len(vertices) - 1)]
+	s = 1
+	dist[s] = 0
+	for i in range(len(vertices) - 1):
+		for e in edges:
+			print('e', e[0], e[1])
+			u, v = e[0], e[1]
+			if dist[v] > dist[u] + 1 and dist[u] != 999999:
+				dist[v] = dist[u] + 1
+				prev[v] = u
+	print('distances: {}\nprevious: {}\n'.format(dist, prev))
 
 
 if __name__ == '__main__':
 	print(instructions)
-	num_vertices = get_num_vertices()
+	vertices = get_vertices()
 	edges = get_edges()
+	bellman_ford(vertices, edges)
+
